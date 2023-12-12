97446512fb871ce409d6387b0d9edbc878f9e61c
sirsparsh <92727010+sirsparsh@users.noreply.github.com>
2023-12-12 15:28:33 +0530

    Delete parentcomponent.js

--- parentcomponent.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ChildComponent from './ChildComponent';
-
-const ParentComponent = () => {
-  const dataToSend = "Hello from Parent";
-
-  return <ChildComponent data={dataToSend} />;
-};
